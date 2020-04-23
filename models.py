90bc4704ae20cff5eac35aab5fcba2c4f9271cb2
Sumukh <sumukhaithal6>
2020-04-23 20:26:01 +0530

    minor changes

--- models.py
+++ models.py
@@ -1,158 +1,320 @@
-import os
-from collections import defaultdict
+from typing import Union
 from addict import Dict
 
-import numpy as np
 import torch
 import torch.nn as nn
 
-from speedrun import BaseExperiment, IOMixin, register_default_dispatch
-from speedrun.logging.wandb import WandBMixin
+import modules as mods
+import attn
 
-from models import ContactTracingTransformer
-from loader import get_dataloader
-from losses import WeightedSum
-from utils import to_device, momentum_accumulator
-from scheduler import GradualWarmupScheduler
-from torch.optim.lr_scheduler import StepLR, ExponentialLR
+# Legend
+#   B: batch (i.e. humans)
+#   T: index of the day
+#   M: message/encounter index
+#   C: channel
 
-class CTTTrainer(WandBMixin, IOMixin, BaseExperiment):
-    WANDB_PROJECT = "ctt"
 
-    def __init__(self):
-        super(CTTTrainer, self).__init__()
-        self.auto_setup()
-        self._build()
+class _ContactTracingTransformer(nn.Module):
+    def __init__(
+        self,
+        *,
+        health_embedding: nn.Module,
+        time_embedding: nn.Module,
+        partner_id_embedding: nn.Module,
+        message_embedding: nn.Module,
+        self_attention_blocks: nn.ModuleList,
+        self_latent_variable_pooler: Union[nn.Module, None],
+        latent_variable_mlp: nn.Module,
+        encounter_logit_sink_pooler: Union[nn.Module, None],
+        logit_sink_mlp: Union[nn.Module, None],
+        encounter_mlp: nn.Module,
+        entity_masker: nn.Module,
+        message_placeholder: nn.Parameter,
+        partner_id_placeholder: nn.Parameter,
+    ):
+        super(_ContactTracingTransformer, self).__init__()
+        self.health_embedding = health_embedding
+        self.time_embedding = time_embedding
+        self.partner_id_embedding = partner_id_embedding
+        self.message_embedding = message_embedding
+        self.self_attention_blocks = self_attention_blocks
+        self.self_latent_variable_pooler = self_latent_variable_pooler
+        self.latent_variable_mlp = latent_variable_mlp
+        self.encounter_logit_sink_pooler = encounter_logit_sink_pooler
+        self.logit_sink_mlp = logit_sink_mlp
+        self.encounter_mlp = encounter_mlp
+        self.entity_masker = entity_masker
+        self.message_placeholder = message_placeholder
+        self.partner_id_placeholder = partner_id_placeholder
 
-    def _build(self):
-        self._build_loaders()
-        self._build_model()
-        self._build_criteria_and_optim()
-        self._build_scheduler()
-
-    def _build_model(self):
-        self.model: nn.Module = to_device(
-            ContactTracingTransformer(**self.get("model/kwargs", {})), self.device
+    def forward(self, inputs):
+        """
+        Parameters
+        ----------
+        inputs : dict
+            A python dict with the following keys:
+                -> `health_history`: a B(T=14)C tensor of the 14-day health
+                    history (symptoms + test results + day) of the individual.
+                -> `history_days`: a B(T=14)1 tensor of the day corresponding to the
+                    T dimension in `health_history`.
+                -> `encounter_health`: a BMC tensor of health during an
+                    encounter indexed by M.
+                -> `encounter_message`: a BMC tensor of the received
+                    message from the encounter partner.
+                -> `encounter_day`: a BM1 tensor of the encounter day.
+                -> `encounter_partner_id`: a binary  BMC tensor specifying
+                    the ID of the encounter partner.
+                -> `mask`: a BM1 mask tensor distinguishing the valid entries (1)
+                    from padding (0) in the set valued inputs.
+        Returns
+        -------
+        Dict
+        """
+        # -------- Shape Wrangling --------
+        batch_size = inputs["health_history"].shape[0]
+        num_history_days = inputs["health_history"].shape[1]
+        num_encounters = inputs["encounter_health"].shape[1]
+        # -------- Embeddings --------
+        # Embed health
+        embedded_health_history = self.health_embedding(inputs["health_history"])
+        embedded_encounter_health = self.health_embedding(
+            inputs["encounter_health"], inputs["mask"]
         )
-
-    def _build_loaders(self):
-        train_path = self.get("data/paths/train", ensure_exists=True)
-        validate_path = self.get("data/paths/validate", ensure_exists=True)
-        self.train_loader = get_dataloader(
-            path=train_path, **self.get("data/loader_kwargs", ensure_exists=True)
+        # Embed time (days)
+        embedded_history_days = self.time_embedding(inputs["history_days"])
+        embedded_encounter_day = self.time_embedding(inputs["encounter_day"], inputs["mask"])
+        # Embed partner-IDs
+        embedded_encounter_partner_ids = self.partner_id_embedding(
+            inputs["encounter_partner_id"], inputs["mask"]
         )
-        self.validate_loader = get_dataloader(
-            path=validate_path, **self.get("data/loader_kwargs", ensure_exists=True)
+        # Embed messages
+        embedded_encounter_messages = self.message_embedding(
+            inputs["encounter_message"], inputs["mask"]
         )
-
-    def _build_criteria_and_optim(self):
-        # noinspection PyArgumentList
-        self.loss = WeightedSum.from_config(self.get("losses", ensure_exists=True))
-        self.optim = torch.optim.Adam(
-            self.model.parameters(), **self.get("optim/kwargs")
+        # -------- Self Attention --------
+        # Prepare the entities -- one set for the encounters and the other for self health
+        encounter_entities = torch.cat(
+            [
+                embedded_encounter_day,
+                embedded_encounter_partner_ids,
+                embedded_encounter_health,
+                embedded_encounter_messages,
+            ],
+            dim=-1,
         )
-
-    def _build_scheduler(self):
-        self.scheduler_steplr = StepLR(self.optim, step_size=10, gamma=0.1)
-        self.scheduler_warmup = GradualWarmupScheduler(self.optim, multiplier=1, total_epoch=5, after_scheduler=self.scheduler_steplr)
-
-    @property
-    def device(self):
-        return self.get("device", "cpu")
-
-    @register_default_dispatch
-    def train(self):
-        self.initialize_wandb()
-        for epoch in self.progress(
-            range(self.get("training/num_epochs", ensure_exists=True)), tag="epochs"
-        ):
-            self.train_epoch()
-            validation_stats = self.validate_epoch()
-            self.log_progress("epochs", **validation_stats)
-            self.scheduler_warmup.step(epoch)
-            self.next_epoch()
-
-    def train_epoch(self):
-        self.clear_moving_averages()
-        self.model.train()
-
-
-        for model_input in self.progress(self.train_loader, tag="train"):
-            # Evaluate model
-            model_input = to_device(model_input, self.device)
-            model_output = Dict(self.model(model_input))
-            # Compute loss
-            losses = self.loss(model_input, model_output)
-            loss = losses.loss
-            self.optim.zero_grad()
-            loss.backward()
-            self.optim.step()
-            # Log to wandb (if required)
-            self.log_training_losses(losses)
-            # Log to pbar
-            self.accumulate_in_cache(
-                "moving_loss", loss.item(), momentum_accumulator(0.9)
+        expanded_message_placeholder = self.message_placeholder[None, None].expand(
+            batch_size, num_history_days, embedded_encounter_messages.shape[-1]
+        )
+        expanded_pid_placeholder = self.partner_id_placeholder[None, None].expand(
+            batch_size, num_history_days, embedded_encounter_partner_ids.shape[-1]
+        )
+        self_entities = torch.cat(
+            [
+                embedded_history_days,
+                expanded_pid_placeholder,
+                embedded_health_history,
+                expanded_message_placeholder,
+            ],
+            dim=2,
+        )
+        # Concatenate encounter and self entities in to one big set (before passing to
+        # the self attention blocks). In addition, expand inputs.mask to account for
+        # masking the entire set of entities.
+        entities = torch.cat([encounter_entities, self_entities], dim=1)
+        extra_mask = torch.ones(
+            size=(batch_size, num_history_days),
+            dtype=inputs["mask"].dtype,
+            device=inputs["mask"].device,
+        )
+        expanded_mask = torch.cat([inputs["mask"], extra_mask], dim=1)
+        entities = self.entity_masker(entities, expanded_mask)
+        # Grab a copy of the "meta-data", which we will be appending to entities at
+        # every step. These meta-data are the time-stamps and partner_ids
+        meta_data_dim = (
+            embedded_history_days.shape[2] + embedded_encounter_partner_ids.shape[2]
+        )
+        meta_data = entities[:, :, :meta_data_dim]
+        # Let'er rip!
+        # noinspection PyTypeChecker
+        for sab in self.self_attention_blocks:
+            entities = sab(entities)
+            entities = self.entity_masker(entities, expanded_mask)
+            # Append meta-data for the next round of message passing
+            entities = torch.cat([meta_data, entities], dim=2)
+        # -------- Entity Pooling --------
+        # -------- Latent Variables
+        # Pool the self entities together to predict one latent variable (if required)
+        if self.self_latent_variable_pooler is not None:
+            pre_latent_variable = self.self_latent_variable_pooler(
+                entities[:, num_encounters:]
             )
-            self.log_progress(
-                "train", loss=self.read_from_cache("moving_loss"),
+            assert pre_latent_variable.shape[1] == 1
+            pre_latent_variable = pre_latent_variable.reshape(
+                batch_size, pre_latent_variable.shape[2]
             )
-            self.next_step()
-
-    def validate_epoch(self):
-        all_losses = defaultdict(list)
-        self.model.eval()
-        for model_input in self.progress(self.validate_loader, tag="validation"):
-            with torch.no_grad():
-                model_input = to_device(model_input, self.device)
-                model_output = Dict(self.model(model_input))
-                losses = self.loss(model_input, model_output)
-                all_losses["loss"].append(losses.loss.item())
-                for key in losses.unweighted_losses:
-                    all_losses[key].append(losses.unweighted_losses[key].item())
-        # Compute mean for all losses
-        all_losses = Dict({key: np.mean(val) for key, val in all_losses.items()})
-        self.log_validation_losses(all_losses)
-        # Store the validation loss in cache. This will be used for checkpointing.
-        self.write_to_cache("current_validation_loss", all_losses.loss)
-        return all_losses
-
-    def log_training_losses(self, losses):
-        if self.log_wandb_now:
-            metrics = Dict({"training_loss": losses.loss})
-            metrics.update(
-                {f"training_{k}": v for k, v in losses.unweighted_losses.items()}
-            )
-            self.wandb_log(**metrics)
-        return self
-
-    def checkpoint(self, force=True):
-        current_validation_loss = self.read_from_cache(
-            "current_validation_loss", float("inf")
-        )
-        best_validation_loss = self.read_from_cache(
-            "best_validation_loss", float("inf")
-        )
-        if current_validation_loss < best_validation_loss:
-            self.write_to_cache("best_validation_loss", current_validation_loss)
-            ckpt_path = os.path.join(self.checkpoint_directory, "best.ckpt")
         else:
-            ckpt_path = None
-        if ckpt_path is not None:
-            info_dict = {
-                "model": self.model.state_dict(),
-                "optim": self.optim.state_dict(),
-            }
-            torch.save(info_dict, ckpt_path)
-        return self
+            pre_latent_variable = entities[:, num_encounters:]
+        # Push through the latent variable MLP to get the latent variables
+        # latent_variable.shape = BC
+        latent_variable = self.latent_variable_mlp(pre_latent_variable)
+        # -------- Logit sink
+        if self.encounter_logit_sink_pooler is not None:
+            pre_logit_sink = self.encounter_logit_sink_pooler(entities)
+            assert pre_logit_sink.shape[1] == 1
+            # logit_sink.shape = B1C
+            assert self.logit_sink_mlp is not None
+            logit_sink = self.logit_sink_mlp(pre_logit_sink)
+        else:
+            assert self.logit_sink_mlp is None
+            logit_sink = torch.zeros(
+                (batch_size, 0, entities.shape[2] - meta_data_dim),
+                dtype=entities.dtype,
+                device=entities.device,
+            )
+        # -------- Generate Output Variables --------
+        # Process encounters to their variables
+        pre_encounter_variables = torch.cat(
+            [entities[:, :num_encounters, meta_data_dim:], logit_sink], dim=1
+        )
+        encounter_variables = self.encounter_mlp(pre_encounter_variables)
+        # Done: pack to an addict and return
+        results = dict()
+        results["encounter_variables"] = encounter_variables
+        results["latent_variable"] = latent_variable
+        return results
 
-    def log_validation_losses(self, losses):
-        metrics = {f"validation_{k}": v for k, v in losses.items()}
-        self.wandb_log(**metrics)
-        return self
 
-    def clear_moving_averages(self):
-        return self.clear_in_cache("moving_loss")
-
-
-if __name__ == "__main__":
-    CTTTrainer().run()
+class ContactTracingTransformer(_ContactTracingTransformer):
+    def __init__(
+        self,
+        *,
+        # Embeddings
+        capacity=128,
+        dropout=0.1,
+        num_health_features=13,
+        health_embedding_dim=64,
+        time_embedding_dim=32,
+        num_encounter_partner_id_bits=16,
+        encounter_partner_id_embedding_dim=32,
+        message_dim=8,
+        message_embedding_dim=128,
+        # Attention
+        num_heads=4,
+        sab_capacity=128,
+        num_sabs=2,
+        # Meta config
+        pool_latent_entities=False,
+        use_logit_sink=False,
+        # Output
+        encounter_output_features=1,
+        latent_variable_output_features=1,
+    ):
+        # ------- Embeddings -------
+        health_embedding = mods.HealthEmbedding(
+            in_features=num_health_features,
+            embedding_size=health_embedding_dim,
+            capacity=capacity,
+            dropout=dropout,
+        )
+        time_embedding = mods.PositionalEncoding(encoding_dim=time_embedding_dim)
+        partner_id_embedding = mods.PartnerIdEmbedding(
+            num_id_bits=num_encounter_partner_id_bits,
+            embedding_size=encounter_partner_id_embedding_dim,
+        )
+        message_embedding = mods.MessageEmbedding(
+            message_dim=message_dim,
+            embedding_size=message_embedding_dim,
+            capacity=capacity,
+            dropout=dropout,
+        )
+        # ------- Attention -------
+        sab_in_dim = (
+            time_embedding_dim
+            + encounter_partner_id_embedding_dim
+            + health_embedding_dim
+            + message_embedding_dim
+        )
+        sab_metadata_dim = time_embedding_dim + encounter_partner_id_embedding_dim
+        sab_intermediate_in_dim = sab_capacity + sab_metadata_dim
+        # Build the SABs
+        if num_sabs >= 1:
+            self_attention_blocks = [
+                attn.SAB(dim_in=sab_in_dim, dim_out=sab_capacity, num_heads=num_heads)
+            ]
+        else:
+            # This is a special code-path where we don't use any self-attention,
+            # but just a plain-old MLP (as a baseline).
+            self_attention_blocks = [
+                nn.Sequential(
+                    nn.Linear(sab_in_dim, sab_capacity),
+                    nn.ReLU(),
+                    nn.Linear(sab_capacity, sab_capacity),
+                    nn.ReLU(),
+                    nn.Linear(sab_capacity, sab_capacity),
+                    nn.ReLU(),
+                )
+            ]
+        for sab_idx in range(num_sabs - 1):
+            self_attention_blocks.append(
+                attn.SAB(
+                    dim_in=sab_intermediate_in_dim,
+                    dim_out=sab_capacity,
+                    num_heads=num_heads,
+                )
+            )
+        self_attention_blocks = nn.ModuleList(self_attention_blocks)
+        # Build the entity poolers
+        if pool_latent_entities:
+            self_latent_variable_pooler = attn.PMA(
+                dim=sab_capacity + sab_metadata_dim, num_seeds=1, num_heads=num_heads
+            )
+        else:
+            self_latent_variable_pooler = None
+        if use_logit_sink:
+            encounter_logit_sink_pooler = attn.PMA(
+                dim=sab_capacity + sab_metadata_dim, num_seeds=1, num_heads=num_heads
+            )
+        else:
+            encounter_logit_sink_pooler = None
+        # ------- Output processors -------
+        # Encounter
+        if use_logit_sink:
+            logit_sink_mlp = nn.Linear(sab_capacity + sab_metadata_dim, sab_capacity)
+        else:
+            logit_sink_mlp = None
+        encounter_mlp = nn.Sequential(
+            nn.Linear(sab_capacity, capacity),
+            nn.ReLU(),
+            nn.Linear(capacity, encounter_output_features),
+        )
+        # Latent variables
+        latent_variable_mlp = nn.Sequential(
+            nn.Linear(sab_capacity + sab_metadata_dim, capacity),
+            nn.ReLU(),
+            nn.Linear(capacity, latent_variable_output_features),
+        )
+        # ------- Output placeholders -------
+        # noinspection PyArgumentList
+        message_placeholder = nn.Parameter(torch.randn((message_embedding_dim,)))
+        # noinspection PyArgumentList
+        partner_id_placeholder = nn.Parameter(
+            torch.randn((encounter_partner_id_embedding_dim,))
+        )
+        # ------- Masking -------
+        entity_masker = mods.EntityMasker()
+        # Done; init the super
+        super(ContactTracingTransformer, self).__init__(
+            health_embedding=health_embedding,
+            time_embedding=time_embedding,
+            partner_id_embedding=partner_id_embedding,
+            message_embedding=message_embedding,
+            self_attention_blocks=self_attention_blocks,
+            self_latent_variable_pooler=self_latent_variable_pooler,
+            latent_variable_mlp=latent_variable_mlp,
+            encounter_logit_sink_pooler=encounter_logit_sink_pooler,
+            logit_sink_mlp=logit_sink_mlp,
+            encounter_mlp=encounter_mlp,
+            entity_masker=entity_masker,
+            message_placeholder=message_placeholder,
+            partner_id_placeholder=partner_id_placeholder,
+        )
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 from collections import defaultdict
 from addict import Dict
 
@@ -6,14 +7,18 @@ import torch
 import torch.nn as nn
 
 from speedrun import BaseExperiment, IOMixin, register_default_dispatch
+from speedrun.logging.wandb import WandBMixin
 
 from models import ContactTracingTransformer
 from loader import get_dataloader
 from losses import WeightedSum
 from utils import to_device, momentum_accumulator
+from scheduler import GradualWarmupScheduler
+from torch.optim.lr_scheduler import StepLR, ExponentialLR
 
+class CTTTrainer(WandBMixin, IOMixin, BaseExperiment):
+    WANDB_PROJECT = "ctt"
 
-class CTTTrainer(IOMixin, BaseExperiment):
     def __init__(self):
         super(CTTTrainer, self).__init__()
         self.auto_setup()
@@ -23,6 +28,7 @@ class CTTTrainer(IOMixin, BaseExperiment):
         self._build_loaders()
         self._build_model()
         self._build_criteria_and_optim()
+        self._build_scheduler()
 
     def _build_model(self):
         self.model: nn.Module = to_device(
@@ -46,31 +52,43 @@ class CTTTrainer(IOMixin, BaseExperiment):
             self.model.parameters(), **self.get("optim/kwargs")
         )
 
+    def _build_scheduler(self):
+        self.scheduler_steplr = StepLR(self.optim, step_size=10, gamma=0.1)
+        self.scheduler_warmup = GradualWarmupScheduler(self.optim, multiplier=1, total_epoch=5, after_scheduler=self.scheduler_steplr)
+
     @property
     def device(self):
         return self.get("device", "cpu")
 
     @register_default_dispatch
     def train(self):
+        self.initialize_wandb()
         for epoch in self.progress(
             range(self.get("training/num_epochs", ensure_exists=True)), tag="epochs"
         ):
             self.train_epoch()
             validation_stats = self.validate_epoch()
             self.log_progress("epochs", **validation_stats)
+            self.scheduler_warmup.step(epoch)
+            self.next_epoch()
 
     def train_epoch(self):
         self.clear_moving_averages()
+        self.model.train()
+
+
         for model_input in self.progress(self.train_loader, tag="train"):
             # Evaluate model
             model_input = to_device(model_input, self.device)
-            model_output = self.model(model_input)
+            model_output = Dict(self.model(model_input))
             # Compute loss
             losses = self.loss(model_input, model_output)
             loss = losses.loss
             self.optim.zero_grad()
             loss.backward()
             self.optim.step()
+            # Log to wandb (if required)
+            self.log_training_losses(losses)
             # Log to pbar
             self.accumulate_in_cache(
                 "moving_loss", loss.item(), momentum_accumulator(0.9)
@@ -78,20 +96,59 @@ class CTTTrainer(IOMixin, BaseExperiment):
             self.log_progress(
                 "train", loss=self.read_from_cache("moving_loss"),
             )
+            self.next_step()
 
     def validate_epoch(self):
         all_losses = defaultdict(list)
+        self.model.eval()
         for model_input in self.progress(self.validate_loader, tag="validation"):
             with torch.no_grad():
                 model_input = to_device(model_input, self.device)
-                model_output = self.model(model_input)
+                model_output = Dict(self.model(model_input))
                 losses = self.loss(model_input, model_output)
                 all_losses["loss"].append(losses.loss.item())
                 for key in losses.unweighted_losses:
                     all_losses[key].append(losses.unweighted_losses[key].item())
         # Compute mean for all losses
         all_losses = Dict({key: np.mean(val) for key, val in all_losses.items()})
+        self.log_validation_losses(all_losses)
+        # Store the validation loss in cache. This will be used for checkpointing.
+        self.write_to_cache("current_validation_loss", all_losses.loss)
         return all_losses
+
+    def log_training_losses(self, losses):
+        if self.log_wandb_now:
+            metrics = Dict({"training_loss": losses.loss})
+            metrics.update(
+                {f"training_{k}": v for k, v in losses.unweighted_losses.items()}
+            )
+            self.wandb_log(**metrics)
+        return self
+
+    def checkpoint(self, force=True):
+        current_validation_loss = self.read_from_cache(
+            "current_validation_loss", float("inf")
+        )
+        best_validation_loss = self.read_from_cache(
+            "best_validation_loss", float("inf")
+        )
+        if current_validation_loss < best_validation_loss:
+            self.write_to_cache("best_validation_loss", current_validation_loss)
+            ckpt_path = os.path.join(self.checkpoint_directory, "best.ckpt")
+        else:
+            ckpt_path = None
+        if ckpt_path is not None:
+            info_dict = {
+                "model": self.model.state_dict(),
+                "optim": self.optim.state_dict(),
+            }
+            torch.save(info_dict, ckpt_path)
+        return self
+
+    def log_validation_losses(self, losses):
+        metrics = {f"validation_{k}": v for k, v in losses.items()}
+        self.wandb_log(**metrics)
+        return self
 
     def clear_moving_averages(self):
         return self.clear_in_cache("moving_loss")
