24921ef032a583ff269ff9ddebd18af01e73996e
nrahaman <nasim.rahaman@tuebingen.mpg.de>
2020-04-24 11:06:54 +0200

    - blacked code - metrics are now logged to wandb

--- metrics.py
+++ metrics.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Metrics(nn.Module):
     def __init__(self):
         super(Metrics, self).__init__()
@@ -20,13 +21,17 @@ class Metrics(nn.Module):
 
     def update(self, model_input, model_output):
         # Task 1: Infectiousness Prediction
-        diff = model_output.latent_variable[:, :, 0:1].view(-1) - model_input.infectiousness_history.view(-1)
+        diff = model_output.latent_variable[:, :, 0:1].view(
+            -1
+        ) - model_input.infectiousness_history.view(-1)
         self.total_infectiousness_loss += torch.sum(diff * diff).item()
         self.total_infectiousness_count += diff.size(0)
 
         # Task 2: Encounter Contagion Prediction
-        # Extract prediction from model_output 
-        prediction = model_output.encounter_variables.squeeze(2).masked_fill((1 - model_input.mask).bool(), -float('inf'))
+        # Extract prediction from model_output
+        prediction = model_output.encounter_variables.squeeze(2).masked_fill(
+            (1 - model_input.mask).bool(), -float("inf")
+        )
         # Extract prediction from model_input
         target = model_input.encounter_is_contagion.squeeze(2)
         for k in range(target.size(0)):
@@ -40,5 +45,15 @@ class Metrics(nn.Module):
             self.total_encounter_count += 1
 
     def evaluate(self):
-        return dict([('mse', math.sqrt(self.total_infectiousness_loss / (self.total_infectiousness_count + 0.001))), 
-            ('mrr', self.total_encounter_mrr / self.total_encounter_count)])
+        return dict(
+            [
+                (
+                    "mse",
+                    math.sqrt(
+                        self.total_infectiousness_loss
+                        / (self.total_infectiousness_count + 0.001)
+                    ),
+                ),
+                ("mrr", self.total_encounter_mrr / self.total_encounter_count),
+            ]
+        )
