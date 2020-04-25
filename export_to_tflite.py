a570d9fb93f95b6e99a6be6325290846901720b6
Pierre Luc <carriepl@leto03.server.mila.quebec>
2020-04-25 14:31:00 -0400

    code cleanup

--- export_to_tflite.py
+++ export_to_tflite.py
@@ -115,25 +115,10 @@ import pdb; pdb.set_trace()
 tflite_model = converter.convert()
 open("model.tflite", "wb").write(tflite_model)
 
-# Sanity-check the TFLite model
-
+# Sanity-check the TFLite model (doesn't work at the moment)
 interpreter = tf.lite.Interpreter(model_path="model.tflite")
 interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
-import pdb; pdb.set_trace()
-
-# Test model on random input data.
-input_shape = input_details[0]['shape']
-input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-interpreter.set_tensor(input_details[0]['index'], input_data)
-
-interpreter.invoke()
-
-# The function `get_tensor()` returns a copy of the tensor data.
-# Use `tensor()` in order to get a pointer to the tensor.
-output_data = interpreter.get_tensor(output_details[0]['index'])
-print(output_data)
-
 import pdb; pdb.set_trace()
 
