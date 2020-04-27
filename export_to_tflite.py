c31849ab61c48184914565bf5898b22d8e2441ca
Pierre Luc <carriepl@eos19.server.mila.quebec>
2020-04-27 09:50:58 -0400

    Implement sanity check for model

--- export_to_tflite.py
+++ export_to_tflite.py
@@ -32,8 +32,6 @@ for i in batch:
     input_names.append(i)
 output_names = ['encounter_variables', 'latent_variable']
 
-print(input_names)
-import pdb; pdb.set_trace()
 # Convert PyTorch model to ONNX format 
 torch.onnx.export(pytorch_model,            
                   batch,                         
@@ -42,26 +40,13 @@ torch.onnx.export(pytorch_model,
                   opset_version=10,          
                   do_constant_folding=True,  
                   input_names=input_names,  
-                  output_names=output_names,
-                  dynamic_axes={
-                    'mask' : {0: 'batch_size', 1: 'sequence'},
-                    'health_history' : {0: 'batch_size'},
-                    'health_profile' : {0: 'batch_size'},
-                    'infectiousness_history' : {0: 'batch_size'},
-                    'history_days' : {0: 'batch_size'},
-                    'current_compartment' : {0: 'batch_size'},
-                    'encounter_health' : {0: 'batch_size', 1: 'sequence'},
-                    'encounter_message' : {0: 'batch_size', 1: 'sequence'},
-                    'encounter_day' : {0: 'batch_size', 1: 'sequence'},
-                    'encounter_partner_id' : {0: 'batch_size', 1: 'sequence'},
-                    'encounter_duration' : {0: 'batch_size', 1: 'sequence'},
-                    'encounter_is_contagion' : {0: 'batch_size', 1: 'sequence'}
-                  }) 
+                  output_names=output_names)
                   
 # Load ONNX model and convert to TF model
 onnx_model = onnx.load("model_onnx_10.onnx")  
 tf_model = prepare(onnx_model)
 
+"""
 # Sanity-check the TF model
 deltas = []
 for i, batch in enumerate(iter(dataloader)):
@@ -80,9 +65,7 @@ print("Tensorflow Model sanity check")
 print("Min abs. diff with pytorch model output : %f" % abs_deltas.min())
 print("Mean abs. diff with pytorch model output : %f" % abs_deltas.mean())
 print("Max abs. diff with pytorch model output : %f" % abs_deltas.max())
-
-# Export TF model as frozen inference graph
-tf_model.export_graph('tf_model.pb')
+"""
 
 # Convert the tf graph to a TF Saved Model
 save_dir = "./tmp_tfmodel_conversion/"
@@ -102,7 +85,7 @@ with tf.compat.v1.Session(graph=tf_model.graph) as sess:
     
     sigs = {signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY :
             tf.compat.v1.saved_model.signature_def_utils.predict_signature_def(input_spec, output_spec)}
-            
+
     builder.add_meta_graph_and_variables(sess,
                                          [tag_constants.SERVING],
                                          signature_def_map=sigs)
@@ -111,14 +94,53 @@ with tf.compat.v1.Session(graph=tf_model.graph) as sess:
 
 # Convert Saved Model to TFLite model
 converter = tf.lite.TFLiteConverter.from_saved_model(save_dir)
-import pdb; pdb.set_trace()
+converter.allow_custom_ops=True
+converter.experimental_new_converter=True
+converter.enable_mlir_converter=True
 tflite_model = converter.convert()
 open("model.tflite", "wb").write(tflite_model)
 
 # Sanity-check the TFLite model (doesn't work at the moment)
 interpreter = tf.lite.Interpreter(model_path="model.tflite")
-interpreter.allocate_tensors()
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-import pdb; pdb.set_trace()
+
+
+# Sanity-check the TF model
+deltas = []
+for i, batch in enumerate(iter(dataloader)):
+    print(i)
+
+    # Resize TFLite inputs to size of batch data
+    interpreter.reset_all_variables()
+    for inp_detail in interpreter.get_input_details():
+        inp_name = inp_detail["name"]
+        inp_shape = list(batch[inp_name].shape)
+        interpreter.resize_tensor_input(inp_detail["index"], inp_shape)
+
+    # Send inputs to the TFLite model
+    interpreter.allocate_tensors()
+    for inp_detail in interpreter.get_input_details():
+        inp_name = inp_detail["name"]
+        interpreter.set_tensor(inp_detail["index"], batch[inp_name])
+
+    # Get TFLite model outputs
+    tflite_output = {}
+    interpreter.invoke()
+    for out_name, out_detail in zip(output_names, interpreter.get_output_details()):
+        tflite_output[out_name] = interpreter.get_tensor(out_detail["index"])
+
+    # Compare with pytorch model output
+    pytorch_output = pytorch_model(batch)
+    for k in pytorch_output.keys():
+        k_delta = pytorch_output[k].detach().numpy() - tflite_output[k]
+        deltas.append(k_delta)
+
+    if i >= NB_EXAMPLES_FOR_SANITY_CHECK:
+        break # Limit the testing to avoid spending too much time on it.
+
+abs_deltas = numpy.abs(numpy.hstack(deltas))
+print("TFLite Model sanity check")
+print("Min abs. diff with pytorch model output : %f" % abs_deltas.min())
+print("Mean abs. diff with pytorch model output : %f" % abs_deltas.mean())
+print("Max abs. diff with pytorch model output : %f" % abs_deltas.max())
+
 
