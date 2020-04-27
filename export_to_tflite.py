6ee705ea88c6fb8317f5d6531b1ceed513503650
Pierre Luc <carriepl@eos19.server.mila.quebec>
2020-04-27 14:58:30 -0400

    Generate multiple fixed-size TFLite models

--- export_to_tflite.py
+++ export_to_tflite.py
@@ -14,133 +14,219 @@ from tensorflow.python.saved_model import tag_constants
 from models import ContactTracingTransformer
 from loader import get_dataloader
 
-NB_EXAMPLES_FOR_SANITY_CHECK=5
+NB_EXAMPLES_FOR_SANITY_CHECK = 100
+NB_MESSAGES_BUCKETS = [10, 50, 100, 1000]
 
-# Load pytorch model
-pytorch_model = ContactTracingTransformer()
-pytorch_model.load_state_dict(torch.load("models/model.pth"))
-pytorch_model.eval()
-
-# Load dataset (used for sanity checking the converted models)
-path = "./data/"
-dataloader = get_dataloader(batch_size=1, shuffle=False, num_workers=0, path=path)
-batch = next(iter(dataloader))
-
-# Get list of inputs names as in the batch
-input_names=[]
-for i in batch:
-    input_names.append(i)
-output_names = ['encounter_variables', 'latent_variable']
-
-# Convert PyTorch model to ONNX format 
-torch.onnx.export(pytorch_model,            
-                  batch,                         
-                  "model_onnx_10.onnx",   
-                  export_params=True,        
-                  opset_version=10,          
-                  do_constant_folding=True,  
-                  input_names=input_names,  
-                  output_names=output_names)
-                  
-# Load ONNX model and convert to TF model
-onnx_model = onnx.load("model_onnx_10.onnx")  
-tf_model = prepare(onnx_model)
-
-"""
-# Sanity-check the TF model
-deltas = []
-for i, batch in enumerate(iter(dataloader)):
-    pytorch_output = pytorch_model(batch)
-    tf_output = tf_model.run(batch)
-    
-    for k in pytorch_output.keys():
-        k_delta = pytorch_output[k].detach().numpy() - getattr(tf_output, k)
-        deltas.append(k_delta)
-    
-    if i >= NB_EXAMPLES_FOR_SANITY_CHECK:
-        break # Limit the testing to avoid spending too much time on it.
-
-abs_deltas = numpy.abs(numpy.hstack(deltas))
-print("Tensorflow Model sanity check")
-print("Min abs. diff with pytorch model output : %f" % abs_deltas.min())
-print("Mean abs. diff with pytorch model output : %f" % abs_deltas.mean())
-print("Max abs. diff with pytorch model output : %f" % abs_deltas.max())
-"""
-
-# Convert the tf graph to a TF Saved Model
-save_dir = "./tmp_tfmodel_conversion/"
-if os.path.isdir(save_dir):
-    print('Already saved a model, cleaning up')
-    shutil.rmtree(save_dir, ignore_errors=True)
-
-builder = tf.compat.v1.saved_model.builder.SavedModelBuilder(save_dir)
-with tf.compat.v1.Session(graph=tf_model.graph) as sess:
-    
-    input_spec = {}
-    output_spec = {}
-    for name in tf_model.inputs:
-        input_spec[name] = tf_model.tensor_dict[name]
-    for name in output_names:
-        output_spec[name] = tf_model.tensor_dict[name]
-    
-    sigs = {signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY :
-            tf.compat.v1.saved_model.signature_def_utils.predict_signature_def(input_spec, output_spec)}
-
-    builder.add_meta_graph_and_variables(sess,
-                                         [tag_constants.SERVING],
-                                         signature_def_map=sigs)
-    builder.save()
+def pad_pytorch_tensor(arr, axis, nb_zeroes):
+    # Pads pytorch tensor with trailing zeroes across a single dimension
+    pad_dimensions = [(0, nb_zeroes) if i==axis else (0, 0) for i in range(arr.ndim)]
+    padded_arr = numpy.pad(arr, pad_dimensions, 'constant', constant_values=0)
+    padded_tensor = torch.Tensor(padded_arr)
+    return padded_tensor
 
 
-# Convert Saved Model to TFLite model
-converter = tf.lite.TFLiteConverter.from_saved_model(save_dir)
-converter.allow_custom_ops=True
-converter.experimental_new_converter=True
-converter.enable_mlir_converter=True
-tflite_model = converter.convert()
-open("model.tflite", "wb").write(tflite_model)
+def pad_messages_minibatch(batch, target_nb_messages):
+    nb_messages = batch['mask'].shape[1]
+    nb_messages_add = target_nb_messages - nb_messages
 
-# Sanity-check the TFLite model (doesn't work at the moment)
-interpreter = tf.lite.Interpreter(model_path="model.tflite")
+    # Initialize padded_batch as copy of batch
+    padded_batch = batch.copy()
 
-
-# Sanity-check the TF model
-deltas = []
-for i, batch in enumerate(iter(dataloader)):
-    print(i)
-
-    # Resize TFLite inputs to size of batch data
-    interpreter.reset_all_variables()
-    for inp_detail in interpreter.get_input_details():
-        inp_name = inp_detail["name"]
-        inp_shape = list(batch[inp_name].shape)
-        interpreter.resize_tensor_input(inp_detail["index"], inp_shape)
-
-    # Send inputs to the TFLite model
-    interpreter.allocate_tensors()
-    for inp_detail in interpreter.get_input_details():
-        inp_name = inp_detail["name"]
-        interpreter.set_tensor(inp_detail["index"], batch[inp_name])
-
-    # Get TFLite model outputs
-    tflite_output = {}
-    interpreter.invoke()
-    for out_name, out_detail in zip(output_names, interpreter.get_output_details()):
-        tflite_output[out_name] = interpreter.get_tensor(out_detail["index"])
-
-    # Compare with pytorch model output
-    pytorch_output = pytorch_model(batch)
-    for k in pytorch_output.keys():
-        k_delta = pytorch_output[k].detach().numpy() - tflite_output[k]
-        deltas.append(k_delta)
-
-    if i >= NB_EXAMPLES_FOR_SANITY_CHECK:
-        break # Limit the testing to avoid spending too much time on it.
-
-abs_deltas = numpy.abs(numpy.hstack(deltas))
-print("TFLite Model sanity check")
-print("Min abs. diff with pytorch model output : %f" % abs_deltas.min())
-print("Mean abs. diff with pytorch model output : %f" % abs_deltas.mean())
-print("Max abs. diff with pytorch model output : %f" % abs_deltas.max())
+    # Pad the inputs whose size depends on the nb of messages
+    padded_batch['mask'] = pad_pytorch_tensor(
+        batch['mask'], axis=1, nb_zeroes=nb_messages_add)
+    padded_batch['encounter_health'] = pad_pytorch_tensor(
+        batch['encounter_health'], axis=1, nb_zeroes=nb_messages_add)
+    padded_batch['encounter_message'] = pad_pytorch_tensor(
+        batch['encounter_message'], axis=1, nb_zeroes=nb_messages_add)
+    padded_batch['encounter_partner_id'] = pad_pytorch_tensor(
+        batch['encounter_partner_id'], axis=1, nb_zeroes=nb_messages_add)
+    padded_batch['encounter_day'] = pad_pytorch_tensor(
+        batch['encounter_day'], axis=1, nb_zeroes=nb_messages_add)
+    padded_batch['encounter_duration'] = pad_pytorch_tensor(
+        batch['encounter_duration'], axis=1, nb_zeroes=nb_messages_add)
+    padded_batch['encounter_is_contagion'] = pad_pytorch_tensor(
+        batch['encounter_is_contagion'], axis=1, nb_zeroes=nb_messages_add)
+    return padded_batch
 
 
+def convert_pytorch_model(pytorch_model, working_directory="./tmp_tfmodel_conversion/"):
+    for nb_messages in NB_MESSAGES_BUCKETS:
+        convert_pytorch_model_fixed_messages(pytorch_model, nb_messages,
+                                             working_directory)
+
+
+def convert_pytorch_model_fixed_messages(pytorch_model, nb_messages,
+                                         working_directory="./tmp_tfmodel_conversion/"):
+
+    # Make sure we are converting the inference graph
+    pytorch_model.eval()
+
+    # Setup working directory
+    if not os.path.exists(working_directory):
+        os.makedirs(working_directory)
+
+    # Load dataset (used for sanity checking the converted models)
+    path = "./data/1k-1-output/"
+    dataloader = get_dataloader(batch_size=1, shuffle=False, num_workers=0, path=path)
+    batch = next(iter(dataloader))
+
+    # Get a padded batch to use for the conversion to TF and TFLite
+    batch = pad_messages_minibatch(batch, nb_messages)
+
+    # Get list of inputs names as in the batch
+    input_names=[]
+    for i in batch:
+        input_names.append(i)
+    output_names = ['encounter_variables', 'latent_variable']
+
+    # Convert PyTorch model to ONNX format
+    onnx_model_path = os.path.join(working_directory, "model_onnx_10.onnx")
+    torch.onnx.export(pytorch_model,
+                      batch,
+                      onnx_model_path,
+                      export_params=True,
+                      opset_version=10,
+                      do_constant_folding=True,
+                      input_names=input_names,
+                      output_names=output_names)
+
+    # Load ONNX model and convert to TF model
+    onnx_model = onnx.load(onnx_model_path)
+    tf_model = prepare(onnx_model)
+
+
+    # Convert the tf graph to a TF Saved Model
+    tf_model_path = os.path.join(working_directory, "tf_model" )
+    if os.path.isdir(tf_model_path):
+        print('Already saved a TF model, cleaning up')
+        shutil.rmtree(tf_model_path, ignore_errors=True)
+
+    builder = tf.compat.v1.saved_model.builder.SavedModelBuilder(tf_model_path)
+    with tf.compat.v1.Session(graph=tf_model.graph) as sess:
+
+        input_spec = {}
+        output_spec = {}
+        for name in tf_model.inputs:
+            input_spec[name] = tf_model.tensor_dict[name]
+        for name in output_names:
+            output_spec[name] = tf_model.tensor_dict[name]
+
+        sigs = {signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY :
+                tf.compat.v1.saved_model.signature_def_utils.predict_signature_def(input_spec, output_spec)}
+
+        builder.add_meta_graph_and_variables(sess,
+                                             [tag_constants.SERVING],
+                                             signature_def_map=sigs)
+        builder.save()
+
+    # Convert Saved Model to TFLite model
+    converter = tf.lite.TFLiteConverter.from_saved_model(tf_model_path)
+    converter.allow_custom_ops=True
+    converter.experimental_new_converter=True
+    converter.enable_mlir_converter=True
+    tflite_model = converter.convert()
+    tflite_model_path = os.path.join(working_directory, "model_%i_messages.tflite" % nb_messages)
+    open(tflite_model_path, "wb").write(tflite_model)
+
+    # Sanity-check the Tensorflow and TFLite models on the examples that have, at most,
+    # the maximum number of messages that they can handle.
+    interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
+    pytorch_tf_deltas = []
+    tf_tflite_deltas = []
+    pytorch_tflite_deltas = []
+    nb_validation_samples = 0
+    for batch in iter(dataloader):
+
+        # If this example is too big for the model, skip it.
+        batch_nb_messages = batch["mask"].shape[1]
+        if batch_nb_messages > nb_messages:
+            continue
+
+        # Generate padded inputs for the TF and TFLite models
+        padded_batch = pad_messages_minibatch(batch, nb_messages)
+
+        # Get pytorch model output
+        pytorch_output = pytorch_model(batch)
+        pytorch_padded_output = pytorch_model(padded_batch)
+
+        # Get TF model output
+        tf_padded_output = tf_model.run(padded_batch)
+        tf_output={
+            "encounter_variables" : tf_padded_output.encounter_variables[:, :batch_nb_messages],
+            "latent_variable" : tf_padded_output.latent_variable
+        }
+
+        # Send inputs to the TFLite model
+        interpreter.allocate_tensors()
+        for inp_detail in interpreter.get_input_details():
+            inp_name = inp_detail["name"]
+            interpreter.set_tensor(inp_detail["index"], padded_batch[inp_name])
+
+        # Get TFLite model outputs
+        tflite_output = {}
+        interpreter.invoke()
+        for out_name, out_detail in zip(output_names, interpreter.get_output_details()):
+            if out_name == "encounter_variables":
+                # Remove the message padding
+                tflite_output[out_name] = interpreter.get_tensor(out_detail["index"])[:, :batch_nb_messages]
+            else:
+                tflite_output[out_name] = interpreter.get_tensor(out_detail["index"])
+
+        # Compare the three models
+        for k in pytorch_output.keys():
+            k_pytorch_tf_delta = pytorch_output[k].detach().numpy() - tf_output[k]
+            pytorch_tf_deltas.append(k_pytorch_tf_delta)
+
+            k_tf_tflite_delta = tf_output[k] - tflite_output[k]
+            tf_tflite_deltas.append(k_tf_tflite_delta)
+
+            k_pytorch_tflite_delta = pytorch_output[k].detach().numpy() - tflite_output[k]
+            pytorch_tflite_deltas.append(k_pytorch_tflite_delta)
+
+        # Limit the testing to avoid spending too much time on it.
+        nb_validation_samples += 1
+        if nb_validation_samples >= NB_EXAMPLES_FOR_SANITY_CHECK:
+            break
+
+
+    # Log the results of the conversion sanity check
+    if nb_validation_samples == 0:
+        raise ValueError("Model generated for %i messages could not be tested. All validation "
+        "samples have too many messages to be used for this model." % nb_messages)
+
+    log_filename = os.path.join(working_directory, "model_%i_messages.txt" % nb_messages)
+    with open(log_filename, "w") as f:
+
+        abs_pytorch_tf_deltas = numpy.abs(numpy.hstack(pytorch_tf_deltas))
+        abs_tf_tflite_deltas = numpy.abs(numpy.hstack(tf_tflite_deltas))
+        abs_pytorch_tflite_deltas = numpy.abs(numpy.hstack(pytorch_tflite_deltas))
+
+        f.write("Models compared on %i validation samples\n\n" % nb_validation_samples)
+
+        f.write("Conversion from pytorch model to TF model\n")
+        f.write("  Min abs diff between outputs : %f \n" % abs_pytorch_tf_deltas.min())
+        f.write("  Mean abs diff between outputs : %f \n" % abs_pytorch_tf_deltas.mean())
+        f.write("  Max abs diff between outputs : %f \n\n" % abs_pytorch_tf_deltas.max())
+
+        f.write("Conversion from TF model to TFLite model\n")
+        f.write("  Min abs diff between outputs : %f \n" % abs_tf_tflite_deltas.min())
+        f.write("  Mean abs diff between outputs : %f \n" % abs_tf_tflite_deltas.mean())
+        f.write("  Max abs diff between outputs : %f \n\n" % abs_tf_tflite_deltas.max())
+
+        f.write("Overall conversion from pytorch model to TFLite model\n")
+        f.write("  Min abs diff between outputs : %f \n" % abs_pytorch_tflite_deltas.min())
+        f.write("  Mean abs diff between outputs : %f \n" % abs_pytorch_tflite_deltas.mean())
+        f.write("  Max abs diff between outputs : %f \n\n" % abs_pytorch_tflite_deltas.max())
+
+    return abs_pytorch_tflite_deltas.max()
+
+
+if __name__ == '__main__':
+    # Load pytorch model
+    pytorch_model = ContactTracingTransformer()
+    pytorch_model.load_state_dict(torch.load("models/model.pth"))
+    pytorch_model.eval()
+
+    # Launch conversion
+    convert_pytorch_model(pytorch_model)
