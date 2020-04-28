35a1da2ea7b73ea2184ec459c535b87d02e39ef2
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2020-04-28 14:03:15 -0400

    Update infworker to support multi-human proc

--- server_utils.py
+++ server_utils.py
@@ -81,22 +81,10 @@ class InferenceWorker(threading.Thread):
             if socket in evts and evts[socket] == zmq.POLLIN:
                 proc_start_time = time.time()
                 address, empty, buffer = socket.recv_multipart()
-                hdi = pickle.loads(buffer)
-                output, human = None, None
-                try:
-                    if 'risk_model' not in hdi or hdi['risk_model'] == "naive":
-                        output, human = proc_human(hdi)
-                    elif hdi['risk_model'] == "transformer":
-                        output, human = proc_human(hdi)
-                        output = engine.infer(output)  # TODO: figure out what to return from output @@@
-                except InvalidSetSize:
-                    pass  # return None for invalid samples
+                response = self.process_sample(buffer, engine)
+                response = pickle.dumps(response)
                 self.packet_counter.increment()
                 self.time_counter.increment(time.time() - proc_start_time)
-                if human is not None:
-                    response = pickle.dumps((human['name'], human['risk'], human['clusters']))
-                else:
-                    response = pickle.dumps(None)
                 socket.send_multipart([address, b"", response])
         socket.close()
 
@@ -114,6 +102,27 @@ class InferenceWorker(threading.Thread):
     def stop(self):
         """Stops the infinite data reception loop, allowing a clean shutdown."""
         self.stop_flag.set()
+
+    @staticmethod
+    def process_sample(hdi, engine):
+        if isinstance(hdi, bytes):
+            hdi = pickle.loads(hdi)
+        if isinstance(hdi, list):
+            return [InferenceWorker.process_sample(h, engine) for h in hdi]
+        output, human = None, None
+        try:
+            if 'risk_model' not in hdi or hdi['risk_model'] == "naive":
+                output, human = proc_human(hdi)
+            elif hdi['risk_model'] == "transformer":
+                output, human = proc_human(hdi)
+                output = engine.infer(output)  # TODO: figure out what to return from output @@@
+        except InvalidSetSize:
+            pass  # return None for invalid samples
+        if human is not None:
+            response = (human['name'], human['risk'], human['clusters'])
+        else:
+            response = None
+        return response
 
 
 class InferenceServerManager(threading.Thread):
