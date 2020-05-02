c02abf1ded2fd1bda17e7954a6f4382763483777
nrahaman <nasim.rahaman@tuebingen.mpg.de>
2020-05-02 21:14:31 +0200

    - create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ctt',
+    version='0.1',
+    packages=['ctt', 'ctt.frozen', 'ctt.models', 'ctt.serving', 'ctt.inference', 'ctt.conversion', 'ctt.data_loading'],
+    url='https://github.com/nasimrahaman/ctt',
+    license='MIT',
+    author='Nasim Rahaman',
+    author_email='nasim.rahaman@tuebingen.mpg.de',
+    description='Contact Tracing Transformer'
+)
