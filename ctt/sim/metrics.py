434022310f0fa5f53581668f0145c9df86bd02cf
nrahaman <nasim.rahaman@tuebingen.mpg.de>
2020-05-13 13:40:07 +0200

    - port some code over to evaluate the metrics

--- ctt/sim/metrics.py
+++ ctt/sim/metrics.py
@@ -1,18 +1,33 @@
 import os
 import zipfile
 import pickle
+from datetime import datetime
 
 from addict import Dict
 import networkx as nx
+import numpy as np
 
 
 class SimulationMetrics(object):
-    def __init__(self, simulator_log_directory):
+    def __init__(
+        self,
+        simulator_log_directory=None,
+        simulation_start=datetime(2020, 2, 28),
+        simulation_stop=datetime(2020, 3, 12),
+    ):
         self.simulator_log_directory = simulator_log_directory
+        self.simulation_start = simulation_start
+        self.simulation_stop = simulation_stop
 
-    def load_logs(self):
+    def load_logs(self, simulator_log_directory=None):
+        simulator_log_directory = (
+            self.simulator_log_directory
+            if simulator_log_directory is None
+            else simulator_log_directory
+        )
+        assert simulator_log_directory is not None
         # Read files in log dir and sort them by time
-        archive_path = os.path.join(self.simulator_log_directory, "data.zip")
+        archive_path = os.path.join(simulator_log_directory, "data.zip")
         with zipfile.ZipFile(archive_path, mode="r") as zf:
             files = sorted(zf.namelist())
             # Load 'em up
@@ -22,13 +37,13 @@ class SimulationMetrics(object):
                     logs.extend(pickle.load(f))
             return logs
 
-    def get_encounter_logs(self):
-        logs = self.load_logs()
+    def get_encounter_logs(self, logs=None, simulator_log_directory=None):
+        logs = self.load_logs(simulator_log_directory) if logs is None else logs
         encounter_logs = [log for log in logs if log["event_type"] == "encounter"]
         return encounter_logs
 
-    def make_encounter_graph(self, encounter_logs=None):
-        encounter_logs = encounter_logs or self.get_encounter_logs()
+    @staticmethod
+    def make_encounter_graph(encounter_logs):
         encounter_graph = nx.MultiDiGraph()
         for encounter_log in encounter_logs:
             # Ids
@@ -37,50 +52,42 @@ class SimulationMetrics(object):
             # Write states
             human1_state = Dict()
             human2_state = Dict()
+            # fmt: off
             # Infection status
-            human1_state.is_infected = (
-                encounter_log["payload"]["unobserved"]["human1"]["infection_timestamp"]
-                is not None
-            )
-            human2_state.is_infected = (
-                encounter_log["payload"]["unobserved"]["human2"]["infection_timestamp"]
-                is not None
-            )
-            human1_state.got_exposed = encounter_log["payload"]["unobserved"]["human1"][
-                "got_exposed"
-            ]
-            human2_state.got_exposed = encounter_log["payload"]["unobserved"]["human2"][
-                "got_exposed"
-            ]
-            human1_state.caused_exposure = encounter_log["payload"]["unobserved"][
-                "human1"
-            ]["exposed_other"]
-            human2_state.caused_exposure = encounter_log["payload"]["unobserved"][
-                "human2"
-            ]["exposed_other"]
+            human1_state.is_infected = \
+                (encounter_log["payload"]["unobserved"]["human1"]["infection_timestamp"]
+                 is not None)
+            human2_state.is_infected = \
+                (encounter_log["payload"]["unobserved"]["human2"]["infection_timestamp"]
+                 is not None)
+            human1_state.got_exposed = \
+                encounter_log["payload"]["unobserved"]["human1"]["got_exposed"]
+            human2_state.got_exposed = \
+                encounter_log["payload"]["unobserved"]["human2"]["got_exposed"]
+            human1_state.caused_exposure = \
+                encounter_log["payload"]["unobserved"]["human1"]["exposed_other"]
+            human2_state.caused_exposure = \
+                encounter_log["payload"]["unobserved"]["human2"]["exposed_other"]
             # Health and symptoms
-            human1_state.is_infected = encounter_log["payload"]["unobserved"]["human1"][
-                "symptoms"
-            ]
-            human2_state.is_infected = encounter_log["payload"]["unobserved"]["human2"][
-                "symptoms"
-            ]
+            human1_state.symptoms = \
+                encounter_log["payload"]["unobserved"]["human1"]["symptoms"]
+            human2_state.symptoms = \
+                encounter_log["payload"]["unobserved"]["human2"]["symptoms"]
             # Risk
-            human1_state.risk = encounter_log["payload"]["unobserved"]["human1"]["risk"]
-            human2_state.risk = encounter_log["payload"]["unobserved"]["human2"]["risk"]
-            human1_state.risk_level = encounter_log["payload"]["unobserved"]["human1"][
-                "risk_level"
-            ]
-            human2_state.risk_level = encounter_log["payload"]["unobserved"]["human2"][
-                "risk_level"
-            ]
+            human1_state.risk = \
+                encounter_log["payload"]["unobserved"]["human1"]["risk"]
+            human2_state.risk = \
+                encounter_log["payload"]["unobserved"]["human2"]["risk"]
+            human1_state.risk_level = \
+                encounter_log["payload"]["unobserved"]["human1"]["risk_level"]
+            human2_state.risk_level = \
+                encounter_log["payload"]["unobserved"]["human2"]["risk_level"]
             # Recommendations
-            human1_state.rec_level = encounter_log["payload"]["unobserved"]["human1"][
-                "rec_level"
-            ]
-            human2_state.rec_level = encounter_log["payload"]["unobserved"]["human2"][
-                "rec_level"
-            ]
+            human1_state.rec_level = \
+                encounter_log["payload"]["unobserved"]["human1"]["rec_level"]
+            human2_state.rec_level = \
+                encounter_log["payload"]["unobserved"]["human2"]["rec_level"]
+            # fmt: on
             # Write stuff out
             encounter_graph.add_edge(
                 human1_id,
@@ -90,6 +97,15 @@ class SimulationMetrics(object):
                 human2_state=human2_state,
             )
         return encounter_graph
+
+    @staticmethod
+    def filter_encounter_graph(graph, from_time, to_time):
+        filtered = nx.MultiDiGraph()
+        filtered.add_nodes_from(graph.nodes)
+        for h1, h2, time in graph.edges:
+            if from_time <= time <= to_time:
+                filtered.add_edge(h1, h2, time, **graph.edges[h1, h2, time])
+        return filtered
 
     @staticmethod
     def filter_edges(graph, condition_fn):
@@ -161,3 +177,89 @@ class SimulationMetrics(object):
         least_recent_date = min(list(edge_spec.keys()))
         least_recent_attries = edge_spec[least_recent_date]
         return least_recent_date, least_recent_attries
+
+    def compute_metrics(
+        self,
+        encounter_logs=None,
+        logs=None,
+        simulator_log_directory=None,
+        simulation_start=None,
+        simulation_stop=None,
+        return_everything=False,
+    ):
+        # Read in the logs
+        encounter_logs = (
+            encounter_logs
+            if encounter_logs is not None
+            else self.get_encounter_logs(logs, simulator_log_directory)
+        )
+        # Build and filter the graph such that the encounters lie between sim start
+        # and stop.
+        simulation_start = (
+            simulation_start if simulation_start is not None else self.simulation_start
+        )
+        simulation_stop = (
+            simulation_stop if simulation_stop is not None else self.simulation_stop
+        )
+        encounter_graph = self.filter_encounter_graph(
+            graph=self.make_encounter_graph(encounter_logs),
+            from_time=simulation_start,
+            to_time=simulation_stop,
+        )
+        # Get the contagion graph with only the ecounters that happened without
+        # symptoms
+        contagion_graph = self.filter_edges(
+            encounter_graph, self.is_contagion_without_symptoms
+        )
+        _attry_extractor = lambda human1_state, human2_state: dict(
+            exposer_risk=human1_state["risk"],
+            exposer_risk_level=human1_state["risk_level"],
+            exposer_rec_level=human1_state["rec_level"],
+        )
+        contagion_digraph = self.to_digraph(contagion_graph, _attry_extractor)
+        encounter_digraph = self.to_concat_digraph(encounter_graph, _attry_extractor)
+        # Read the risks from the graph
+        raw_risks_all = [
+            _risk
+            for u, v in encounter_digraph.edges
+            for _risk in encounter_digraph[u][v]["exposer_risk"]
+        ]
+        raw_risks_at_exposure = [
+            contagion_digraph[u][v]["exposer_risk"] for u, v in contagion_digraph.edges
+        ]
+        metrics = self._evaluate_metrics_given_risks(
+            raw_risks_all=raw_risks_all, raw_risks_at_exposure=raw_risks_at_exposure
+        )
+        if return_everything:
+            everything = Dict()
+            everything.encounter_graph = encounter_graph
+            everything.contagion_graph = contagion_graph
+            everything.encounter_digraph = encounter_digraph
+            everything.contagion_digraph = contagion_digraph
+            everything.raw_risks_all = raw_risks_all
+            everything.raw_risks_at_exposure = raw_risks_at_exposure
+            everything.metrics = metrics
+            return everything
+        else:
+            return metrics
+
+    @staticmethod
+    def _evaluate_metrics_given_risks(raw_risks_all, raw_risks_at_exposure):
+        thresholds = sorted(
+            list(np.linspace(min(raw_risks_all), max(raw_risks_all) + 1 / 100, 100))
+            + [np.percentile(raw_risks_at_exposure, x) for x in np.linspace(0, 100, 20)]
+            + [np.percentile(raw_risks_all, x) for x in np.linspace(0, 100, 20)]
+        )
+        raw_risks_above_thresh = np.array(
+            [(raw_risks_all >= thresh).mean() for thresh in thresholds]
+        )
+        exposure_risk_above_thresh = np.array(
+            [(raw_risks_at_exposure >= thresh).mean() for thresh in thresholds]
+        )
+        output = Dict()
+        output.raw_risks_above_thresh = raw_risks_above_thresh
+        output.exposure_risk_above_thresh = exposure_risk_above_thresh
+        output.thresholds = thresholds
+        return output
+
+    __call__ = compute_metrics
