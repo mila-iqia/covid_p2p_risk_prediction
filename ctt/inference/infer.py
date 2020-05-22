0f072b12d3626d370e23876aa1a1c332d7fb25c9
nrahaman <nasim.rahaman@tuebingen.mpg.de>
2020-05-22 16:44:18 +0200

    - inference code now supports DCTTs - code for profiling models

--- ctt/inference/infer.py
+++ ctt/inference/infer.py
@@ -36,9 +36,7 @@ class InferenceEngine(BaseExperiment):
         else:
             assert os.path.exists(path)
             model_cls = getattr(tr, self.get("model/name", "ContactTracingTransformer"))
-            model: torch.nn.Module = model_cls(
-                **self.get("model/kwargs", {})
-            )
+            model: torch.nn.Module = model_cls(**self.get("model/kwargs", {}))
             state = torch.load(path, map_location=torch.device("cpu"))
             model.load_state_dict(state["model"])
         model.eval()
@@ -59,3 +57,31 @@ class InferenceEngine(BaseExperiment):
             )
             infectiousness = model_output["latent_variable"].numpy()[0, :, 0]
         return dict(contagion_proba=contagion_proba, infectiousness=infectiousness)
+
+
+def _profile(num_trials, experiment_directory):
+    from ctt.data_loading.loader import ContactDataset
+    import time
+
+    print("Loading data...")
+    data_path = "../../data/sim_v2_people-1000_days-30_init-0.003_seed-0_20200509-182246-output.zip"
+    dataset = ContactDataset(path=data_path, preload=True)
+    human_day_infos = [dataset.read(file_name=dataset._files[k]) for k in range(num_trials)]
+
+    engine = InferenceEngine(experiment_directory)
+    _ = engine.infer(human_day_infos[0])
+
+    print(f"Profiling {experiment_directory}...")
+    start = time.time()
+    for human_day_info in human_day_infos:
+        _ = engine.infer(human_day_info)
+    stop = time.time()
+
+    print(f"Average time ({num_trials} trials): {(stop - start)/num_trials} s.")
+
+
+if __name__ == "__main__":
+    # _profile(500, "/Users/nrahaman/Python/ctt/tmp/DEBUG-DCTT-MOREXA-0")
+    _profile(1000, "/Users/nrahaman/Python/ctt/tmp/FRESH-SNOWFLAKE-224B")
+    _profile(1000, "/Users/nrahaman/Python/ctt/tmp/WINTER-MOON-285")
+
