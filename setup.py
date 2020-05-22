7b1b86f6f7403e897901d3eff1cedf501ace1ae7
Gaetan Marceau Caron <gaetan.marceau.caron@mila.quebec>
2020-05-22 17:36:08 -0400

    removing missing ctt/frozen and ctt/serving

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='ctt',
     version='0.1',
-    packages=['ctt', 'ctt.frozen', 'ctt.models', 'ctt.serving', 'ctt.inference', 'ctt.conversion', 'ctt.data_loading'],
+    packages=['ctt', 'ctt.models', 'ctt.inference', 'ctt.conversion', 'ctt.data_loading'],
     url='https://github.com/nasimrahaman/ctt',
     license='MIT',
     author='Nasim Rahaman',
