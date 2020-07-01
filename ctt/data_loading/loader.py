f8e8a0121858693d22b1376b138b9d4dff2f17be
nrahaman <nasim.rahaman@tuebingen.mpg.de>
2020-07-01 14:52:50 +0200

    - rename exposed_at_day to exposure_history which is more consistent - update debug config

--- ctt/data_loading/loader.py
+++ ctt/data_loading/loader.py
@@ -434,7 +434,7 @@ class ContactDataset(Dataset):
         viral_load_history = self._fetch_viral_load_history(
             infectiousness_history, human_day_info
         )
-        exposed_at_day = self._fetch_exposed_at_day(human_day_info)
+        exposure_history = self._fetch_exposure_history(human_day_info)
         history_days = np.arange(day_idx - 13, day_idx + 1)[::-1, None]
         valid_history_mask = (history_days >= 0)[:, 0]
         if mask_head:
@@ -495,7 +495,7 @@ class ContactDataset(Dataset):
             sex=torch.from_numpy(sex).float(),
             infectiousness_history=torch.from_numpy(infectiousness_history).float(),
             viral_load_history=torch.from_numpy(viral_load_history).float(),
-            exposed_at_day=torch.from_numpy(exposed_at_day).float(),
+            exposure_history=torch.from_numpy(exposure_history).float(),
             history_days=torch.from_numpy(history_days).float(),
             valid_history_mask=torch.from_numpy(valid_history_mask).float(),
             current_compartment=torch.from_numpy(current_compartment).float(),
@@ -584,12 +584,12 @@ class ContactDataset(Dataset):
             viral_load_history = infectiousness_history
         return viral_load_history
 
-    def _fetch_exposed_at_day(self, human_day_info):
+    def _fetch_exposure_history(self, human_day_info):
         exposed_since = human_day_info["unobserved"]["exposure_day"]
-        exposed_at_day = np.zeros(shape=(14,))
+        exposure_history = np.zeros(shape=(14,))
         if exposed_since is not None and exposed_since < 14:
-            exposed_at_day[exposed_since] = 1
-        return exposed_at_day[:, None]
+            exposure_history[exposed_since] = 1
+        return exposure_history[:, None]
 
     def _fetch_encounter_message(self, encounter_message, num_encounters):
         if self.bit_encoded_messages:
